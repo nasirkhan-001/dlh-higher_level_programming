c93338c1d3e213a5be13f0a73217f0bf6202ead2
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-10 00:19:01 +0000

    indentation correction

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,9 +8,10 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0)
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
-        return self.__size * self.__size
+        area = self.__size * self.__size
+        return area
