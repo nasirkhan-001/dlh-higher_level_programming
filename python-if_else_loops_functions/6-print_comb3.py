6468f729e781248efe4ad2fcb960016df222d35d
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-12 23:03:38 +0000

    unique combination of two digit number, both digit must not be equal, 01=10, choose 89 compared to 89 as last number bcz its smallest, use nested-loop and nested-if

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(1, 10):
-        if i == j:
-            continue
-        elif i < j and i != 8:
-            print("{}".format(i) + "{}".format(j) + ", ", end="")
-        elif i < j and i == 8:
-            print("{}".format(i) + "{}".format(j))
+    for j in range(10):
+        if i < j:
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j))
+            else:
+                print("{}{}".format(i, j), end=" , ")
