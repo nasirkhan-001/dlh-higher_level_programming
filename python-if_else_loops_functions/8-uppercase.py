99db61f8e07ca6d891335650c30d66d71a8d611c
Nasir9372 <@nasirkhan9372@gmail.comgit config --global user.email nasirkhan9372@gmail.comgit config --global user.email @nasirkhan9372@gmail.com>
2026-05-13 09:11:16 +0000

    correction

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,5 @@ def uppercase(str):
         if ord(ch) >= 97 and ord(ch) <= 122:
             ch = chr(ord(ch) - 32)
             print("{}".format(ch), end="")
-    print()
+        else:
+            print("{}".format(ch), end="")
